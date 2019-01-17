572e6bc8b33ed3bc0040de1823aca9556ea4407f
ATruglio <45845488+ATruglio@users.noreply.github.com>
2019-01-17 10:12:27 +0000

    Delete sfera.py

--- sfera.py
+++
@@ -1,9 +0,0 @@
-import math
-math.pi
-print("Inserire il raggio della sfera")
-r=input()
-r=int(r)
-v=(4/3)*math.pi*r**3
-s=4*math.pi*r**2
-print("Il volume della sfera e ",v)
-print("La superfice della sfera e ",s)
