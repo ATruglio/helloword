e0c5c9bd97d1125d61120e81bac6ec4ae4bdfff6
ATruglio <45845488+ATruglio@users.noreply.github.com>
2019-01-19 16:47:25 +0000

    Add files via upload

---
+++ secgrado.py
@@ -0,0 +1,35 @@
+import math
+print("inserire il coefficente di secondo grado")
+a=input()
+a=int(a)
+if(a==0):
+	print("E una equazione di primo grado")
+else:
+	print("Inserire il coefficente di primo grado ")
+	b=input()
+	b=int(b)
+	print("Inserire il termine noto")
+	c=input()
+	c=int(c)
+	if(b==0):
+		if(c<0):
+			x1=int(-(math.sqrt(-c/a)))
+			x2=int(-x1)
+			print("I risultati dell incognita sono ",x1,"  ",x2)
+		else:
+			print("L equazione non ammette soluzioni reali")
+	elif(c==0):
+		x1=0
+		x2=int(-b/a)
+		print("I risultati dell incognita sono ",x1,x2)
+	else:
+		d=b**2-4*a*c
+		if(d==0):
+			x=int(-b/2*a)
+			print("L equazione ha due soluzioni reali e coincidenti e sono" ,x)
+		elif(d<0):
+			print("L equazione non ammette soluzioni reali")
+		else:
+			x1=int((-b-(math.sqrt(d))/2*a))
+			x2=int((-b+(math.sqrt(d))/2*a))
+			print("I risultati dell incognita sono ",x1," ",x2)
